757d24403c5e85964be26dd4148b1f0016a314b8
Med-games <mohamed.idriss@amplyfigroup.com>
2024-01-19 20:10:17 +0100

    remove duplicate "(1)" from names

--- Blogger (1)/src (1)/blog (1)/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post,Comment,video
-from embed_video.admin import AdminVideoMixin
-
-class AdminVideo(AdminVideoMixin,admin.ModelAdmin):
-    pass
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(video,AdminVideo)
